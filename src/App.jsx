aeccc457e76f7e9998db940ed0441356cb338a5a
Dh-Pardeep <pardeepjangra514@gmail.com>
2024-01-06 10:32:19 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import React from 'react'
+import './App.css'
+import MyNavBar from './components/common/MyNavBar'
+function App() {
+  
+  return (
+    <>
+    <div>
+        <MyNavBar/>
+      </div> 
+    </>
+  )
+}
+
+export default App
